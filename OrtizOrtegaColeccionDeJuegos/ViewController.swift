4c6776b3fa1a126f96e205115183dcdb8e84d80a
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2024-10-02 16:47:09 -0500

    Initial Commit

---
+++ OrtizOrtegaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrtizOrtegaColeccionDeJuegos
+//
+//  Created by Bernie Mauro Ortiz Ortega on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
